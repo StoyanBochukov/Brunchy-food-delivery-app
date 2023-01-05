0eddbcf141f425edccdc6eba0cf5fad05fadd8c8
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2023-01-05 18:00:15 +0000

    build: adding media queries

--- src/components/ProductsSlider.jsx
+++ src/components/ProductsSlider.jsx
@@ -1,15 +1,45 @@
 import React from 'react'
 import products from '../data.json'
 import CardItem from './CardItem'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 
 const ProductsSlider = () => {
-
-  
   return (
     <div className='slider-wrapper'>
-      <Swiper className='slider'
+      <Splide
+        aria-label='My Favorite Images'
+        options={{
+          rewind: true,
+          width: '100%',
+          gap: '-5rem',
+          arrows: false,
+          perPage: 6,
+          breakpoints: {
+            1780:{
+              perPage: 5
+            },
+            1500: {
+              perPage: 4,
+            },
+            995: {
+              perPage: 3,
+            },
+            585: {
+              perPage: 2,
+            },
+          },
+        }}
+        className='slider'
+      >
+        {products.map((product) => (
+          <SplideSlide key={product.id} className='slider-slide'>
+            <CardItem product={product} />
+          </SplideSlide>
+        ))}
+      </Splide>
+
+      {/* <Swiper className='slider'
       spaceBetween={50}
       slidesPerView={6}
       onSlideChange={() => console.log('slide change')}
@@ -21,8 +51,8 @@ const ProductsSlider = () => {
         </SwiperSlide>
 
       ))}
-    </Swiper>
-        {/* {products.map(product => (
+    </Swiper> */}
+      {/* {products.map(product => (
           <CardItem product={product} key={product.id} />
         ))} */}
     </div>
