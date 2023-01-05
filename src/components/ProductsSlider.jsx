cb78cdb9fd3d5434ad0801a6fdc448c7b6b23ef9
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2023-01-05 01:05:32 +0000

    build: adding products slider

--- src/components/ProductsSlider.jsx
+++ src/components/ProductsSlider.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import products from '../data.json'
 import CardItem from './CardItem'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 const ProductsSlider = () => {
 
   
   return (
     <div className='slider-wrapper'>
-        {products.map(product => (
+      <Swiper className='slider'
+      spaceBetween={50}
+      slidesPerView={6}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {products.map(product => (
+        <SwiperSlide key={product.id} className='slider-slide'>
+          <CardItem product={product} />
+        </SwiperSlide>
+
+      ))}
+    </Swiper>
+        {/* {products.map(product => (
           <CardItem product={product} key={product.id} />
-        ))}
+        ))} */}
     </div>
   )
 }
